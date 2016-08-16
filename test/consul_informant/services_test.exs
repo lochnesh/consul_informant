27fe14b604bcc6e20b919508eff6cd92a45d0669
Skyler Nesheim <skylernesheim@gmail.com>
2016-08-16 15:44:35 -0500

    MASTER: clean up services_test

--- test/consul_informant/services_test.exs
+++ test/consul_informant/services_test.exs
@@ -2,7 +2,7 @@ defmodule ServicesTest do
   use ExUnit.Case
 
   test "initialize service list" do
-    expected_services = ['test']
+    expected_services = ["test"]
     {:producer_consumer, services, opts} = Services.init(expected_services)
     assert services === expected_services
     assert opts === [subscribe_to: [{ServiceProducer, max_demand: 10, min_demand: 1}]]
@@ -12,7 +12,7 @@ defmodule ServicesTest do
     new = ["service-a", "service-b"]
     current = ["service-1", "service-2"]
     expected = current ++ new
-    {:noreply, events, state} = Services.handle_events(new, "unused", current)
+    {:noreply, events, state} = Services.handle_events(new, nil, current)
     assert events === expected
     assert state === expected
   end
@@ -21,7 +21,7 @@ defmodule ServicesTest do
     new = ["3", "1", "2", "4"]
     current = ["2", "3", "1"]
     expected = ["2", "3", "1", "4"]
-    {:noreply, events, state} = Services.handle_events(new, "u", current)
+    {:noreply, events, state} = Services.handle_events(new, nil, current)
     assert events === expected
     assert state === expected
   end
